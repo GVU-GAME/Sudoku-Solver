604b0132ccd5e17168478495329189cc572f1b34
GVU-GAME <56762234+GVU-GAME@users.noreply.github.com>
2021-07-06 21:56:23 -0400

    for loop to get a number for each square

--- solver.cpp
+++ solver.cpp
@@ -14,6 +14,7 @@ solver::solver() {
     solve(grid);
 }
 
+//checks for empty space, increments the row and col too the empty space
 bool solver::isEmpty(int grid[MAX][MAX], int &row, int &col) {
     for(row = 0; row < MAX; row++) {
         for(col = 0; col < MAX; col++) {
@@ -29,8 +30,15 @@ bool solver::isEmpty(int grid[MAX][MAX], int &row, int &col) {
 //returns if the board is solved or not
 bool solver::isSolved(int grid[MAX][MAX]) {
     int row, col;
+
+    //checks if there is any empty spaces left
     if(!isEmpty(grid, row, col)){
         return true;
+    }
+
+    //gets numbers 1 to 9 to place on the board
+    for(int num = 1; num <= MAX; num++) {
+        //code
     }
 }
 
